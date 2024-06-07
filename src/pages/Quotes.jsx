77f458ce0857336006a50c063da89f8f44d61f13
254mathenge <mathengefaith49@gmail.com>
2024-06-07 09:01:02 +0300

    commit changes

--- src/pages/Quotes.jsx
+++ src/pages/Quotes.jsx
@@ -1,50 +1,52 @@
+/* eslint-disable react-hooks/rules-of-hooks */
 /* eslint-disable no-undef */
-import "./Quotes.css"
+import "./Quotes.css";
 
-import { useState } from 'react';
+import { useState } from "react";
 
 const Quotes = () => {
-    
-    // eslint-disable-next-line no-unused-vars
-    const [quotes, setQuote] = useState("Everyday is a learning day")
-    const [loading,setLoading] = useState(false);
+  // eslint-disable-next-line no-unused-vars
+  const [quotes, setQuote] = useState("Everyday is a learning day");
+  const [loading, setLoading] = useState(false);
 
-    async function handleGetQuote(event) {
-        event.preventDefault;
+  async function handleGetQuote(event) {
+    event.preventDefault;
 
-        const api_url = "https://api.adviceslip.com/advice"
+    const api_url = "https://api.adviceslip.com/advice";
 
-       
-        try{
-         setLoading(true) 
+    try {
+      setLoading(true);
 
-        const result = await fetch(api_url)
-        const response = await result.json()
-        
-        console.log(response.slip,quotes);  
+      const result = await fetch(api_url);
+      const response = await result.json();
 
-        setQuote(response.slip);
+      console.log(response.slip, quotes);
 
-        setLoading(false)
-        }catch(err){
-          console.log(err)
-        }
-        
-        // eslint-disable-next-line no-undef
-        
+      setQuote(response.slip);
+
+      setLoading(false);
+    } catch (err) {
+      console.log(err);
     }
+    useEffect(() => {
+      fetchData;
+    }, []);
 
-    return (
-        <div className="random-quotes">
-            <h1 className="h1">Random Inspirational Quotes</h1>
-             <p className="quote"> {loading ?  "Loading..." : quotes} </p>
-            <div className="get-quote">
-                <form>
-                    <button onClick={handleGetQuote} className="button">Get Quote</button>
-                </form>
-            </div>
-        </div>
-        
-    )
-}
+    // eslint-disable-next-line no-undef
+  }
+
+  return (
+    <div className="random-quotes">
+      <h1 className="h1">Random Inspirational Quotes</h1>
+      <p className="quote"> {loading ? "Loading..." : quotes} </p>
+      <div className="get-quote">
+        <form>
+          <button onClick={handleGetQuote} className="button">
+            Get Quote
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+};
 export default Quotes;
