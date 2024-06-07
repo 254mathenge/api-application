d5ee807bee3ca51a030bfaa9f5725b77e160a2a0
254mathenge <mathengefaith49@gmail.com>
2024-06-07 10:01:27 +0300

    commit changes

--- src/pages/Quotes.jsx
+++ src/pages/Quotes.jsx
@@ -1,44 +1,38 @@
 /* eslint-disable react-hooks/rules-of-hooks */
 /* eslint-disable no-undef */
 import "./Quotes.css";
-
+import { PulseLoader } from "react-spinners";
 import { useState } from "react";
 
 const Quotes = () => {
   // eslint-disable-next-line no-unused-vars
-  const [quotes, setQuote] = useState("Everyday is a learning day");
-  const [loading, setLoading] = useState(false);
+  const [quotes, setQuote] = useState(null);
+  const [loading, setLoading] = useState();
 
   async function handleGetQuote(event) {
     event.preventDefault;
 
-    const api_url = "https://api.adviceslip.com/advice";
-
     try {
+      const api_url = "https://api.adviceslip.com/advice";
       setLoading(true);
 
       const result = await fetch(api_url);
       const response = await result.json();
-
-      console.log(response.slip, quotes);
-
-      setQuote(response.slip);
-
       setLoading(false);
+      setQuote(response.slip.advice);
+
     } catch (err) {
-      console.log(err);
+      setQuote("error fetching data");
+      setLoading(false);
     }
-    useEffect(() => {
-      fetchData;
-    }, []);
 
     // eslint-disable-next-line no-undef
   }
-
   return (
     <div className="random-quotes">
       <h1 className="h1">Random Inspirational Quotes</h1>
-      <p className="quote"> {loading ? "Loading..." : quotes} </p>
+      <p> {loading && <PulseLoader color="white" size={40} /> }</p>
+      <p>{quotes}</p>
       <div className="get-quote">
         <form>
           <button onClick={handleGetQuote} className="button">
